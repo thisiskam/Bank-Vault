1de87ea9f7141de1e3fc136d5b0aa50bbc699a62
Kameron Pete <kameroncpete@gmail.com>
2024-01-30 17:19:09 +0000

    first push

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+1. create the html File
+2. link this js file to the indexe.html file
+3. create string for the user
+4. create a variable for each number
+5. use addition to get to 10 for first variable
+6. use subtraction to get to 40 for second variable
+7. use multiplication to get to 39 for third number
+8. make sure to use comments when writing code in JS
+9. use string concadenation to make an alert dispating the string and the numbers.
+10. create github repository
+11. push assignment to git hub
+*/
+
+//message string
+let string = 'You have received this message becase you have been chosen to open an important vault. Here is the secret combination: ';
+
+//first number of combination
+let num1 = 5 + 5;
+
+//second number of combination
+let num2 = 100 - 60;
+
+//third number of combination
+let num3 = 13 * 3;
+
+//alert message will pop up in browser
+alert(string + num1 + ' - ' + num2 + ' - ' + num3 + '.')
